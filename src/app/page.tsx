056a55c40cd7fb9678f24d262cfd622e8a5aed7a
Eman Radwan <emanalaa11499@gmail.com>
2026-05-31 17:43:15 +0300

    refactor: edit css variables' names TM-02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <h1 className="text-display-lg">Taskly</h1>
+      <h1 className="text-display-lg gap-1.75">Taskly</h1>
       <h2 className="text-headline-lg">Taskly</h2>
       <h3 className="text-title-md">Taskly</h3>
       <p className="text-body-md">Taskly</p>
       <p className="text-label-sm">Taskly</p>
-      <button className="primary-gradient rounded-lg">btn</button>
+      <button className="primary-gradient rounded-4px p-8px">btn</button>
     </div>
   );
 }

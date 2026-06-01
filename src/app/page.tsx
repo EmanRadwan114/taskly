3edec195f002544350111d25ec178560493e27bf
Eman Radwan <emanalaa11499@gmail.com>
2026-06-01 19:45:14 +0300

    feat(ui): handle input for password TM-03

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Input from '@/components/ui/Input';
 
 export default function Home() {
   return (
-    <div className="container mx-auto flex flex-col gap-2 w-52">
+    <div className="container mx-auto flex flex-col gap-2 w-80">
       <h1 className="text-display-lg gap-1.75">Taskly</h1>
       <h2 className="text-headline-lg">Taskly</h2>
       <h3 className="text-title-md">Taskly</h3>
@@ -26,6 +26,11 @@ export default function Home() {
         placeholder="write your email"
         fieldMsg="should have 5 chars"
         isOptional
+      />
+      <FormField
+        label="Password"
+        placeholder="write your password"
+        type="password"
       />
       <FormField
         label="Email"

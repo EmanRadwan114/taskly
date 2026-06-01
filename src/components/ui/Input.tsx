653f004c9c2a2c9972e887ce01894a78b4bb875b
Eman Radwan <emanalaa11499@gmail.com>
2026-06-01 19:07:02 +0300

    chore: setup svgr to handle svg icons TM-03

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,4 +1,5 @@
 import React, { InputHTMLAttributes } from 'react';
+import EyeShow from '@/assets/icons/eye-show.svg';
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
   variant?: 'default' | 'error';
@@ -12,10 +13,15 @@ const Input: React.FC<IProps> = ({ variant = 'default', ...props }) => {
       'bg-error-background text-error-dark focus-within:outline-error-dark focus-visible:outline-error-dark',
   };
   return (
-    <input
-      className={`w-full px-16px py-14px rounded-4px focus-within:outline-1 focus-visible:outline-1 ${inputVariants[variant]} ${props.className}`}
-      {...props}
-    />
+    <div
+      className={`w-full flex justify-between items-center gap-2px px-16px py-14px rounded-4px focus-within:outline-1 focus-visible:outline-1 ${inputVariants[variant]} ${props.className}`}
+    >
+      <input
+        className={`w-full focus-within:outline-0 focus-visible:outline-0`}
+        {...props}
+      />
+      <EyeShow />
+    </div>
   );
 };
 

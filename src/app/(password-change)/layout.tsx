7fee876440dd4c183a0cf57befc769d1c1485829
Eman Radwan <emanalaa11499@gmail.com>
2026-06-04 23:01:43 +0300

    fix: fix auth forms ui issues TM-04

--- src/app/(password-change)/layout.tsx
+++ src/app/(password-change)/layout.tsx
@@ -7,19 +7,19 @@ export default async function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div
-      className={`flex flex-col min-h-screen relative bg-secondary-background`}
-    >
-      <header className="py-6.5">
-        <Container>
-          <Logo />
-        </Container>
-      </header>
-      <main className="w-full flex flex-col items-center justify-center pb-48px md:py-48px md:max-w-5/6 lg:max-w-4/6 md:mx-auto xl:max-w-1/2 flex-1">
-        <Container className="flex-1 flex flex-col justify-center items-center">
-          {children}
-        </Container>
-      </main>
+    <div className={`relative bg-secondary-background min-h-screen`}>
+      <div className="md:min-h-[90vh] min-h-screen w-full flex flex-col flex-1 h-full">
+        <header className="py-6.5">
+          <Container>
+            <Logo />
+          </Container>
+        </header>
+        <main className="w-full flex flex-col items-center justify-center pb-48px md:py-48px md:max-w-5/6 lg:max-w-4/6 md:mx-auto xl:max-w-1/2 flex-1">
+          <Container className="flex-1 flex flex-col justify-center items-center">
+            {children}
+          </Container>
+        </main>
+      </div>
     </div>
   );
 }
--- src/features/auth/components/forget-password/ResetPasswordForm.tsx
+++ src/features/auth/components/forget-password/ResetPasswordForm.tsx
@@ -128,7 +128,7 @@ const ResetPasswordForm: React.FC<IProps> = ({ accessToken }) => {
   };
 
   return (
-    <section className="w-full space-y-10 md:rounded-8px md:p-48px md:shadow-form md:bg-white 2xl:max-w-3/4 2xl:mx-auto">
+    <section className="w-full space-y-10 md:rounded-8px md:p-48px md:shadow-form md:bg-white sm:max-w-3/4 2xl:max-w-3/4 sm:mx-auto">
       <header className="space-y-8px self-start text-center md:text-start">
         <h1 className="form-headline">Create a New Password</h1>
         <p className="text-slate-md">

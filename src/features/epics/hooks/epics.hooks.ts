251012485fe35b54d704051f872de3151ec27474
Eman Radwan <emanalaa11499@gmail.com>
2026-06-12 20:32:55 +0300

    fix: fix passing project_id bug TM-15

--- src/features/epics/hooks/epics.hooks.ts
+++ src/features/epics/hooks/epics.hooks.ts
@@ -1,11 +1,11 @@
-import { projectAction } from '@/features/projects/server-actions/project.actions';
 import { useActionState, useEffect, useTransition } from 'react';
 import { toast } from 'react-toastify';
 import { TEpicsInput } from '../validation/validation.epics';
+import { createEpicAction } from '../server-actions/epics.actions';
 
 // ^ ---------------------------- Create epic Hook ------------------------- //
-export const useCreateEpic = (projectId?: string) => {
-  const action = projectAction.bind(null, projectId);
+export const useCreateEpic = (projectId: string) => {
+  const action = createEpicAction.bind(null, projectId);
 
   const [state, formAction, isPending] = useActionState(action, null);
   const [_, startTransition] = useTransition();
@@ -20,11 +20,10 @@ export const useCreateEpic = (projectId?: string) => {
   }, [state]);
 
   // handlers
-  const onHandleSubmitEpic = (data: TEpicsInput & { project_id: string }) => {
+  const onHandleSubmitEpic = (data: TEpicsInput) => {
     const formData = new FormData();
     if (data.title) formData.append('title', data.title);
     if (data.description) formData.append('description', data.description);
-    if (data.project_id) formData.append('project_id', data.project_id);
     if (data.assignee_id) formData.append('assignee_id', data.assignee_id);
     if (data.deadline) formData.append('deadline', data.deadline);
 
--- src/features/epics/server-actions/epics.actions.ts
+++ src/features/epics/server-actions/epics.actions.ts
@@ -6,7 +6,7 @@ import { revalidatePath } from 'next/cache';
 import { createEpic } from '../services/epics.services';
 
 // ^ ------------------------- Create Epic Action ------------------------- //
-export const CreateEpicAction = async (
+export const createEpicAction = async (
   projectId: string | undefined,
   _: unknown,
   formData: FormData
@@ -15,20 +15,40 @@ export const CreateEpicAction = async (
   const cookieStore = await cookies();
   const accessToken = cookieStore.get(ACCESS_TOKEN_KEY)?.value;
 
-  const values = {
-    title: (formData.get('title') as string) || '',
-    description: (formData.get('description') as string) || '',
-    project_id: (formData.get('project_id') as string) || '',
-    assignee_id: (formData.get('assignee_id') as string) || '',
-    deadline: (formData.get('deadline') as string) || '',
+  const title = formData.get('title') as string;
+  const description = formData.get('description') as string;
+  const assignee_id = formData.get('assignee_id') as string;
+  const deadline = formData.get('deadline') as string;
+
+  const values: {
+    title: string;
+    description?: string;
+    assignee_id?: string;
+    deadline?: string;
+  } = {
+    title,
   };
+
+  if (description) {
+    values.description = description;
+  }
+
+  if (assignee_id) {
+    values.assignee_id = assignee_id;
+  }
+
+  if (deadline) {
+    values.deadline = deadline;
+  }
+
+  console.log(values, projectId);
 
   try {
     if (!accessToken) return;
 
-    if (values.title && values.project_id) {
-      await createEpic({ data: values, accessToken });
-      revalidatePath(`/project/${values.project_id}/epics`);
+    if (values.title && projectId) {
+      await createEpic({ data: values, accessToken, projectId });
+      revalidatePath(`/project/${projectId}/epics`);
       revalidatePath('/epics');
     }
 

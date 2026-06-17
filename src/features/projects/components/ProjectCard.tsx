8d405f865ae0c9e3171c89fc333865c33b55b647
Eman Radwan <emanalaa11499@gmail.com>
2026-06-17 21:01:21 +0300

    refactor: edit project card actions ui TM-17

--- src/features/projects/components/ProjectCard.tsx
+++ src/features/projects/components/ProjectCard.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 import { IProject } from '../types/project.types';
 import Link from 'next/link';
 import EditIcon from '@/assets/icons/edit.svg';
+import Button from '@/shared/components/ui/Button';
+import DotsIcon from '@/assets/icons/dots.svg';
 
 interface IProps {
   project: IProject;
 }
 
 const ProjectCard: React.FC<IProps> = ({ project }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const projectFormatedDate = new Date(project.created_at).toLocaleDateString(
     'en-GB',
     {
@@ -18,14 +24,43 @@ const ProjectCard: React.FC<IProps> = ({ project }) => {
   );
 
   return (
-    <div className="flex flex-col gap-y-3.5 rounded-lg p-6 bg-white">
+    <div
+      className="relative flex flex-col gap-y-3.5 rounded-lg p-6 bg-white"
+      onClick={() => setIsMenuOpen(false)}
+    >
+      {/* actions  */}
+      <header className="flex items-center justify-between">
+        <h2 className="text-title-md text-slate-dark capitalize">
+          {project?.name}
+        </h2>
+        <Button
+          variant="ghost"
+          className="p-1! justify-end w-fit! "
+          onClick={(e) => {
+            e.stopPropagation();
+            setIsMenuOpen((prev) => !prev);
+          }}
+        >
+          <DotsIcon className="w-1 text-slate-dark/20" />
+        </Button>
+        <ul
+          className={`absolute list-none bg-background shadow-sm rounded-sm p-2 top-14 inset-e-2.5 w-26 ${isMenuOpen ? 'block' : 'hidden'}`}
+        >
+          <li>
+            <Link
+              href={`/project/${project?.id}/edit`}
+              className="flex items-center gap-2 p-0.5"
+            >
+              <EditIcon className="w-5 text-primary" />
+              <span className="text-body-md">Edit</span>
+            </Link>
+          </li>
+        </ul>
+      </header>
       <Link
         href={`/project/${project?.id}/epics`}
         className="w-full flex flex-col gap-y-3.5"
       >
-        <h2 className="text-title-md text-slate-dark capitalize">
-          {project?.name}
-        </h2>
         <p className="mb-6">{project?.description}</p>
         <div className="flex justify-between items-end mt-auto">
           <span className="font-bold text-label text-secondary-light uppercase letter-spacing-sm">
@@ -36,14 +71,6 @@ const ProjectCard: React.FC<IProps> = ({ project }) => {
           </span>
         </div>
       </Link>
-      <div className="flex gap-2 justify-end">
-        <Link
-          href={`/project/${project?.id}/edit`}
-          className="flex justify-end p-0.5"
-        >
-          <EditIcon className="w-5 text-primary" />
-        </Link>
-      </div>
     </div>
   );
 };

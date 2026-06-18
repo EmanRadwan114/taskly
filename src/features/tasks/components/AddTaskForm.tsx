2380427c8fe84a6f027c1240a2981acfbeff722a
Eman Radwan <emanalaa11499@gmail.com>
2026-06-18 20:52:03 +0300

    feat: add creat task ui TM-20

--- src/features/tasks/components/AddTaskForm.tsx
+++ src/features/tasks/components/AddTaskForm.tsx
@@ -56,21 +56,79 @@ const ProjectForm: React.FC<IProps> = ({ projectItem }) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* form fields */}
-      <div className="flex flex-col gap-9">
-        {/* name */}
+      <div className="flex flex-col gap-8">
+        {/* title */}
         <div className="flex flex-col gap-1.5 md:col-span-2">
           <Label
-            htmlFor="name"
-            activeVariant={errors.name ? 'error' : 'default'}
+            htmlFor="title"
+            activeVariant={errors.title ? 'error' : 'default'}
           >
-            project title
+            title
             <span className="text-error"> *</span>
           </Label>
           <FormField
             control={control}
-            name="name"
-            label="name"
-            placeholder="Enter project title"
+            name="title"
+            label="title"
+            placeholder="e.g., Finalize structural schematics"
+          />
+        </div>
+        <div className="flex flex-col lg:flex-row gap-8">
+          {/* status */}
+          <div className="flex flex-col gap-1.5 md:col-span-2">
+            <Label
+              htmlFor="status"
+              activeVariant={errors.status ? 'error' : 'default'}
+            >
+              status
+              <span className="text-error"> *</span>
+            </Label>
+            <FormField
+              control={control}
+              name="status"
+              label="status"
+              isSelect
+            />
+          </div>
+          {/* assignee */}
+          <div className="flex flex-col gap-1.5 md:col-span-2">
+            <Label
+              htmlFor="assignee_id"
+              activeVariant={errors.assignee_id ? 'error' : 'default'}
+            >
+              assignee
+            </Label>
+            <FormField
+              control={control}
+              name="assignee_id"
+              label="assignee"
+              isSelect
+            />
+          </div>
+        </div>
+        {/* epic */}
+        <div className="flex flex-col gap-1.5 md:col-span-2">
+          <Label
+            htmlFor="epic_id"
+            activeVariant={errors.epic_id ? 'error' : 'default'}
+          >
+            Epic
+          </Label>
+          <FormField control={control} name="epic_id" label="epic" isSelect />
+        </div>
+        {/* due date */}
+        <div className="flex flex-col gap-1.5 md:col-span-2">
+          <Label
+            htmlFor="due_date"
+            activeVariant={errors.due_date ? 'error' : 'default'}
+          >
+            Due date
+          </Label>
+          <FormField
+            control={control}
+            name="due_date"
+            label="due_date"
+            type="date"
           />
         </div>
         {/* description */}
@@ -87,38 +145,27 @@ const ProjectForm: React.FC<IProps> = ({ projectItem }) => {
             control={control}
             name="description"
             label="description"
-            placeholder={`Provide a high-level overview of the project's architectural objectives and key milestones...`}
+            placeholder={`Provide detailed context for this task...`}
             isTextArea
           />
-          <span className="text-label block text-end font-medium text-slate-md">
-            {descriptionWatcher?.length || 0}/500 characters
-          </span>
         </div>
         {/* actions */}
         <div className="flex flex-col lg:flex-row justify-between items-end gap-4">
           <Button
             variant="ghost"
             type="button"
-            onClick={() =>
-              !isEditMode ? router.back() : router.push('/project')
-            }
+            onClick={() => router.back()}
             className="lg:w-fit! font-bold text-slate-md! text-base! order-1 lg:order-0"
             disabled={isPending}
           >
-            {isEditMode ? 'Cancel' : 'Back'}
+            Back
           </Button>
           <Button
             type="submit"
-            className="lg:w-fit! text-base!"
+            className="lg:w-fit! font-bold text-base!"
             disabled={isPending}
           >
-            {isPending
-              ? isEditMode
-                ? 'Saving Changes...'
-                : 'Creating Project...'
-              : isEditMode
-                ? 'Save Changes'
-                : 'Create Project'}
+            {isPending ? 'Creating Task...' : 'Create Task'}
           </Button>
         </div>
       </div>

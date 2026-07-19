77f8df5492f0fabae6845ac17183cbd53db8647d
Eman Radwan <emanalaa11499@gmail.com>
2026-07-19 19:43:04 +0300

    refactor: edit statistics ui TM-31

--- src/features/statistics/components/StatsCalenderDay.tsx
+++ src/features/statistics/components/StatsCalenderDay.tsx
@@ -2,6 +2,7 @@ import { format, isBefore, isPast, isToday, startOfDay } from 'date-fns';
 import { TaskStatusEnum } from '@/features/tasks/types/tasks.types';
 import { statusBadgeStyle } from '@/features/tasks/utils/tasks.utils';
 import { formateTaskStatus } from '@/shared/utils/functions.client.utils';
+import NoTasksIcon from '@/assets/icons/no-tasks-calendar.svg';
 
 interface IProps {
   dailyStats: { day: string; statuses: Record<TaskStatusEnum, number> };
@@ -19,22 +20,34 @@ const StatsCalenderDay: React.FC<IProps> = ({ dailyStats }) => {
     startOfDay(new Date())
   );
 
-  return (
+  const desktopView = (
     <div
-      className={`bg-white rounded-lg min-h-[40vh] flex flex-col gap-3 w-48 p-3 ${isTodayStats ? 'border-2 border-primary' : ''} ${isPastStats ? 'opacity-60' : ''}`}
+      className={`hidden lg:flex bg-white rounded-lg min-h-[50vh] flex-col gap-3 w-48 flex-1 p-3 relative shadow-primary ${isTodayStats ? 'border-2 border-primary' : ''} ${isPastStats ? 'opacity-60' : ''}`}
     >
+      {isTodayStats && (
+        <div className="bg-primary text-white uppercase font-bold text-body-xs leading-3.75 flex justify-center items-center py-0.5 px-2 w-fit m-auto rounded-full absolute inset-s-0 inset-e-0 top-0 -translate-y-1/2">
+          Today
+        </div>
+      )}
       {/* date */}
-      <div className="flex flex-col gap-1.5 w-full items-center">
-        <span>{weekDay}</span>
-        <div className="flex gap-1 justify-center">
+      <div className="flex flex-col gap-1.5 w-full">
+        <span
+          className={`font-bold leading-4 text-body-sm uppercase ${isTodayStats ? 'text-primary' : 'text-slate-dark/40'}`}
+        >
+          {weekDay}
+        </span>
+        <div
+          className={`flex gap-1 leading-7 text-slate-dark ${isTodayStats ? 'text-heading-5 font-extrabold' : 'text-heading-6 font-bold'}`}
+        >
           <span>{dayNum}</span>
           <span>{month}</span>
         </div>
       </div>
       {/* status tasks */}
       {Object.keys(dailyStats?.statuses || {}).length === 0 ? (
-        <div className="flex items-center justify-center h-full">
-          <span className="text-body-md font-medium text-secondary-light capitalize">
+        <div className="flex flex-col gap-2 items-center justify-center h-full">
+          <NoTasksIcon className="text-secondary/40 w-6.5" />
+          <span className="text-body-sm font-medium text-secondary-light uppercase">
             no tasks
           </span>
         </div>
@@ -44,14 +57,74 @@ const StatsCalenderDay: React.FC<IProps> = ({ dailyStats }) => {
             key={status}
             className={`flex items-center justify-between p-2 rounded-md ${statusBadgeStyle[status]} `}
           >
-            <span className="text-body-xs">
+            <span className="text-label-sm  uppercase text-clip">
               {formateTaskStatus(status as TaskStatusEnum)}
             </span>
-            <span>{count}</span>
+            <span className="text-slate-dark font-bold leading-4 text-body-sm">
+              {count}
+            </span>
           </div>
         ))
       )}
     </div>
+  );
+
+  const mobileView = (
+    <div
+      className={`flex items-center lg:hidden rounded-lg gap-4 p-3 shadow-primary ${isTodayStats ? 'border-s-2 border-s-primary bg-white' : 'bg-surface-low'} ${isPastStats ? 'opacity-60' : ''}`}
+    >
+      {/* date */}
+      <div className="flex flex-col gap-1.5">
+        <span
+          className={`font-bold leading-4 text-body-sm ${isTodayStats ? 'text-primary' : 'text-slate-dark/40'}`}
+        >
+          {weekDay}
+        </span>
+        <div
+          className={`flex gap-1 leading-7  ${isTodayStats ? 'text-heading-5 font-extrabold text-primary' : 'text-heading-6 font-bold text-slate-dark/60'}`}
+        >
+          <span>{dayNum}</span>
+          <span>{month}</span>
+        </div>
+      </div>
+      {/* separator */}
+      <div className="h-10 bg-slate-light/30 w-px" />
+      {/* status tasks */}
+      {Object.keys(dailyStats?.statuses || {}).length === 0 ? (
+        <div className="flex gap-2 items-center justify-center">
+          <NoTasksIcon className="text-slate-dark/60 w-3" />
+          <span className="text-body-xs font-medium text-secondary-light capitalize">
+            no tasks
+          </span>
+        </div>
+      ) : (
+        <div className="flex flex-col gap-1.5 items-center">
+          {statusArray?.map(([status, count]) => (
+            <div
+              key={status}
+              className={`flex items-center justify-between py-0.5 px-1.5 rounded-xs cursor-default ${statusBadgeStyle[status]} `}
+              title={`${count} ${formateTaskStatus(status as TaskStatusEnum)} tasks`}
+            >
+              <span className="text-slate-dark font-bold leading-3.5 text-body-sm">
+                {count}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
+      {isTodayStats && (
+        <div className="bg-primary text-white uppercase text-body-xs font-bold ms-auto px-2 py-0.5 rounded-full">
+          Today
+        </div>
+      )}
+    </div>
+  );
+
+  return (
+    <>
+      {desktopView}
+      {mobileView}
+    </>
   );
 };
 
--- src/features/statistics/components/TasksStatsChart.tsx
+++ src/features/statistics/components/TasksStatsChart.tsx
@@ -4,42 +4,68 @@ import { taskStatusOptions } from '@/features/tasks/data/tasks.data';
 import { TaskStatusEnum } from '@/features/tasks/types/tasks.types';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Doughnut } from 'react-chartjs-2';
+import { IStatusKeys } from '../types/statistics.types';
+import { statusBadgeStyle } from '@/features/tasks/utils/tasks.utils';
+import { formateTaskStatus } from '@/shared/utils/functions.client.utils';
+import { useMobile } from '@/shared/hooks/shared.hooks';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 // Define type for the status object
 interface TaskStatusProps {
-  totals:
-    | {
-        [TaskStatusEnum.TODO]: number;
-        [TaskStatusEnum.IN_PROGRESS]: number;
-        [TaskStatusEnum.DONE]: number;
-        [TaskStatusEnum.BLOCKED]: number;
-        [TaskStatusEnum.IN_REVIEW]: number;
-        [TaskStatusEnum.READY_FOR_PRODUCTION]: number;
-        [TaskStatusEnum.REOPENED]: number;
-        [TaskStatusEnum.READY_FOR_QA]: number;
-      }
-    | undefined;
+  totals: IStatusKeys | undefined;
+  totalTasks: number | undefined;
 }
 
-export default function TasksStatsChart({ totals }: TaskStatusProps) {
+const emptyStatus = {
+  [TaskStatusEnum.TODO]: 0,
+  [TaskStatusEnum.IN_PROGRESS]: 0,
+  [TaskStatusEnum.DONE]: 0,
+  [TaskStatusEnum.BLOCKED]: 0,
+  [TaskStatusEnum.IN_REVIEW]: 0,
+  [TaskStatusEnum.READY_FOR_PRODUCTION]: 0,
+  [TaskStatusEnum.REOPENED]: 0,
+  [TaskStatusEnum.READY_FOR_QA]: 0,
+};
+
+const statusBgStyle = {
+  [TaskStatusEnum.TODO]: 'rgba(255, 99, 132, 1)',
+  [TaskStatusEnum.IN_PROGRESS]: 'rgba(54, 162, 235, 1)',
+  [TaskStatusEnum.DONE]: 'rgba(75, 192, 192, 1)',
+  [TaskStatusEnum.BLOCKED]: 'rgba(255, 165, 0, 1)',
+  [TaskStatusEnum.IN_REVIEW]: 'rgba(128, 0, 128, 1)',
+  [TaskStatusEnum.READY_FOR_PRODUCTION]: 'rgba(255, 192, 203, 1)',
+  [TaskStatusEnum.REOPENED]: 'rgba(0, 100, 0, 1)',
+  [TaskStatusEnum.READY_FOR_QA]: 'rgba(139, 69, 19, 1)',
+};
+
+export default function TasksStatsChart({
+  totals,
+  totalTasks,
+}: TaskStatusProps) {
+  const { isMobile } = useMobile(1024);
   const data = {
-    labels: Object.keys(totals || {}),
+    labels:
+      Object.keys(totals || emptyStatus).length > 0
+        ? Object.keys(totals || emptyStatus)
+        : ['no tasks'],
     datasets: [
       {
         label: 'Tasks Count',
-        data: Object.values(totals || {}),
-        cutout: '70%',
+        data:
+          Object.values(totals || emptyStatus).length > 0
+            ? Object.values(totals || emptyStatus)
+            : [1],
+        cutout: isMobile ? '80%' : '70%',
         backgroundColor: [
-          'rgba(255, 99, 132, 0.5)',
-          'rgba(54, 162, 235, 0.4)',
-          'rgba(75, 192, 192, 0.5)',
-          'rgba(255, 165, 0, 0.5)',
-          'rgba(128, 0, 128, 0.5)',
-          'rgba(255, 192, 203, 0.5)',
-          'rgba(0, 100, 0, 0.5)',
-          'rgba(139, 69, 19, 0.5)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 165, 0, 1)',
+          'rgba(128, 0, 128, 1)',
+          'rgba(255, 192, 203, 1)',
+          'rgba(0, 100, 0, 1)',
+          'rgba(139, 69, 19, 1)',
         ],
         borderColor: [
           'rgba(255, 99, 132, 1)',
@@ -66,11 +92,59 @@ export default function TasksStatsChart({ totals }: TaskStatusProps) {
     },
   };
 
+  const tasksByStatus = Object.entries(totals || emptyStatus);
+  console.log(tasksByStatus);
+
   return (
-    <div className="bg-white shadow-form p-8 rounded-lg">
-      <div style={{ width: '250px', height: '250px' }}>
-        <Doughnut data={data} options={options} />
+    <section className="bg-white shadow-form p-8 rounded-lg flex flex-col gap-5 lg:gap-10">
+      <h3 className="capitalize text-slate-dark font-bold text-heading-6 leading-7">
+        Tasks by status
+      </h3>
+      <div className="flex gap-4 items-center">
+        {/* chart */}
+        <div
+          style={{
+            width: isMobile ? '150px ' : '250px',
+            height: isMobile ? '150px ' : '250px',
+          }}
+        >
+          <Doughnut data={data} options={options} />
+        </div>
+        {/* progress bar */}
+        <div className="flex flex-col gap-2 flex-1">
+          {tasksByStatus?.map(([key, value]) => (
+            <div className="flex flex-col gap-1" key={key}>
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  {/* bullet */}
+                  <div
+                    className={`size-3 rounded-full`}
+                    style={{
+                      backgroundColor: statusBgStyle[key as TaskStatusEnum],
+                    }}
+                  ></div>
+                  {/* status */}
+                  <span className="text-body-sm font-bold text-slate-dark/70 leading-4">
+                    {formateTaskStatus(key as TaskStatusEnum)}
+                  </span>
+                </div>
+                {/* count */}
+                <span className="text-slate-dark font-bold text-body-sm leading-4">
+                  {value}
+                </span>
+              </div>
+              {!isMobile && (
+                <div className="rounded-xl h-1 w-full bg-slate-ligher">
+                  <div
+                    className={`rounded-xl h-1 ${statusBgStyle[key as TaskStatusEnum]}`}
+                    style={{ width: `${(value / (totalTasks || 1)) * 100}%` }}
+                  ></div>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
